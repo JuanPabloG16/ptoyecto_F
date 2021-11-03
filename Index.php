4c5b3439bd86e4f7d416ea271789677cb44bf129
JuanPabloG16 <93049031+JuanPabloG16@users.noreply.github.com>
2021-11-03 18:08:14 -0500

    Initial commit

---
+++ Index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charshet="UTF-8">
+        <title>Estudiantes</title>
+    </head>
+    <body>
+       <header></header>
+       
+    </body>
+</html>
