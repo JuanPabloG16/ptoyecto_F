7710b0276b2f93e6ca1888a84a7e11439fdc16ae
JuanPabloG16 <93049031+JuanPabloG16@users.noreply.github.com>
2021-11-14 14:20:23 -0500

    Primeros pasos

--- Index.php
+++ Index.php
@@ -5,7 +5,22 @@
         <title>Estudiantes</title>
     </head>
     <body>
-       <header></header>
-       
+       <header>
+       <header>
+        <a id="inicio" href="" type="button">INICIO</a>
+        <br>
+        <br>
+        <p id="nom">Nombre: Juan Pablo Gutierrez Henao <br> Codigo: 55220014</p>
+        <a id="b1" href="" type="button">1</a>
+        <a id="b2" href="" type="button">2</a>
+        <a id="b3" href="" type="button">3</a>
+        <a id="b4" href="" type="button">4</a>
+        <a id="b5" href="" type="button">5</a>
+        <a id="b7" href="" type="button">7</a>
+
+
+    </header>
+       </header>
+
     </body>
 </html>
